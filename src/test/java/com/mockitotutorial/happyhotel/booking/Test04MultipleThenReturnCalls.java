ce2526cf388f491470a00702e285edf5bbb59c0f
Taylor <taylor@baremetalcloud.com>
2021-08-27 03:29:12 -0400

    Created test case for multiple return calls of the same method.

---
+++ src/test/java/com/mockitotutorial/happyhotel/booking/Test04MultipleThenReturnCalls.java
@@ -0,0 +1,55 @@
+package com.mockitotutorial.happyhotel.booking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class Test04MultipleThenReturnCalls {
+    private BookingService bookingService;
+    private PaymentService paymentServiceMock;
+    private RoomService roomServiceMock;
+    private BookingDAO bookingDAOMock;
+    private MailSender mailSenderMock;
+
+    @BeforeEach
+    void setUp() {
+
+        this.paymentServiceMock = mock(PaymentService.class);
+        this.roomServiceMock = mock(RoomService.class);
+        this.bookingDAOMock = mock(BookingDAO.class);
+        this.mailSenderMock = mock(MailSender.class);
+
+
+        this.bookingService = new BookingService(paymentServiceMock, roomServiceMock, bookingDAOMock, mailSenderMock);
+
+    }
+
+    @Test
+    void should_CountAvailablePlaces_When_CalledMultipleTimes() {
+        // Given
+        when(this.roomServiceMock.getAvailableRooms())
+                .thenReturn(Collections.singletonList(new Room("Room 1", 5)))
+                .thenReturn(Collections.emptyList());
+        int expectedFirstCall = 5;
+        int expectedSecondCall = 0;
+
+        // When
+        int actualFirst = bookingService.getAvailablePlaceCount();
+        int actualSecond = bookingService.getAvailablePlaceCount();
+
+        // Then
+        assertAll(
+                () -> assertEquals(expectedFirstCall, actualFirst),
+                () -> assertEquals(expectedSecondCall, actualSecond)
+        );
+    }
+
+}
