f776e06175932356756dbae34fe902cc4ff4df4f
Taylor <taylor@baremetalcloud.com>
2021-08-27 03:39:45 -0400

    Created a new test handle when an exception is throw.

---
+++ src/test/java/com/mockitotutorial/happyhotel/booking/Test05ThrowingExceptions.java
@@ -0,0 +1,49 @@
+package com.mockitotutorial.happyhotel.booking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class Test05ThrowingExceptions {
+
+    private BookingService bookingService;
+    private PaymentService paymentServiceMock;
+    private RoomService roomServiceMock;
+    private BookingDAO bookingDAOMock;
+    private MailSender mailSenderMock;
+
+    @BeforeEach
+    void setUp() {
+
+        this.paymentServiceMock = mock(PaymentService.class);
+        this.roomServiceMock = mock(RoomService.class);
+        this.bookingDAOMock = mock(BookingDAO.class);
+        this.mailSenderMock = mock(MailSender.class);
+
+
+        this.bookingService = new BookingService(paymentServiceMock, roomServiceMock, bookingDAOMock, mailSenderMock);
+    }
+
+    @Test
+    void should_ThrowException_When_NoRoomAvailable() {
+        // Given
+        BookingRequest bookingRequest = new BookingRequest("1",
+                LocalDate.of(2020, 01, 01),
+                LocalDate.of(2020, 01, 05), 2, false);
+        when(this.roomServiceMock.findAvailableRoomId(bookingRequest))
+                .thenThrow(BusinessException.class);
+
+        // When
+        Executable executable = () -> bookingService.makeBooking(bookingRequest);
+
+        // Then
+        assertThrows(BusinessException.class, executable);
+    }
+}
