7cddb86161763754808ad7f04e94bc0ec0dd3496
Taylor <taylor@baremetalcloud.com>
2021-08-27 03:07:08 -0400

    Created test for default values also checking out what the mocked methods are giving to the unit test cases.

---
+++ src/test/java/com/mockitotutorial/happyhotel/booking/Test02DefaultReturnValues.java
@@ -0,0 +1,46 @@
+package com.mockitotutorial.happyhotel.booking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class Test02DefaultReturnValues {
+    private BookingService bookingService;
+    private PaymentService paymentServiceMock;
+    private RoomService roomServiceMock;
+    private BookingDAO bookingDAOMock;
+    private MailSender mailSenderMock;
+
+    @BeforeEach
+    void setUp() {
+
+        this.paymentServiceMock = mock(PaymentService.class);
+        this.roomServiceMock = mock(RoomService.class);
+        this.bookingDAOMock = mock(BookingDAO.class);
+        this.mailSenderMock = mock(MailSender.class);
+
+
+        this.bookingService = new BookingService(paymentServiceMock, roomServiceMock, bookingDAOMock, mailSenderMock);
+
+        System.out.println("List return " + roomServiceMock.getAvailableRooms());
+        System.out.println("Object returned " + roomServiceMock.findAvailableRoomId(null));
+        System.out.println("Primitive returned " + roomServiceMock.getRoomCount());
+    }
+
+    @Test
+    void should_CountAvailablePlaces() {
+        // Given
+        int expected = 0;
+
+        // When
+        int actual = bookingService.getAvailablePlaceCount();
+
+        // Then
+        assertEquals(expected, actual);
+
+    }
+}
